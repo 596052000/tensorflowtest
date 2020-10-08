d4fe051b0110b8f482d1f0e680fd206fa44d735c
xuyi <596052000@qq.com>
2020-10-08 14:08:01 +0800

    first commit

---
+++ number one.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+
+import matplotlib.pyplot as plt
+from tensorflow import keras
+from tensorflow.keras import layers
+import numpy as np
+
+
+fashion_mnist = keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+plt.figure()
+plt.imshow(train_images[1])
+plt.colorbar()
+plt.grid(False)
+plt.show()
